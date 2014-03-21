70f0c25671db13b6faa23be7143970d72acdc899
kratein <dylan.bonin@free.fr>
2014-03-21 11:43:37 +0100

    1er commit

---
+++ combobox.js
@@ -0,0 +1,7 @@
+    <script type="text/javascript" language="javascript">
+    function combobox(){
+	select = document.getElementById("CATEGORIE");
+	choix = select.selectedIndex+1;  
+	return choix;
+    }
+    </script>
